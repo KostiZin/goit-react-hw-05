47e6e0edf41e5e55fc0f8eca7159936e2dc1ae1c
Kostia Zinchenko <pancostian@gmail.com>
2024-09-30 17:27:32 -0400

    final-commit-2

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,5 +1,6 @@
 import { Field, Form, Formik } from "formik";
 import s from "./SearchForm.module.css";
+import toast, { Toaster } from "react-hot-toast";
 
 const SearchForm = ({ handleChangeQuery }) => {
   const initialValues = {
@@ -7,14 +8,17 @@ const SearchForm = ({ handleChangeQuery }) => {
   };
 
   const handleSubmit = (values) => {
-    handleChangeQuery(values.query);
+    values.query === ""
+      ? toast.error("Please, type the name of a movie")
+      : handleChangeQuery(values.query);
   };
 
   return (
     <div>
+      <Toaster position="top-right" reverseOrder={false} />
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         <Form className={s.form}>
-          <Field className={s.input} name="query" />
+          <Field className={s.input} name="query" placeholder="Type here..." />
           <button className={s.btn} type="submit">
             Search
           </button>
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -3,23 +3,18 @@ import MovieList from "../../components/MovieList/MovieList";
 import { useSearchParams } from "react-router-dom";
 import SearchForm from "../../components/SearchForm/SearchForm";
 import { fetchSearchMovie } from "../../services/api";
-import { Hourglass } from "react-loader-spinner";
 
 const MoviesPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get("query") ?? "";
   const [movies, setMovies] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (!query) return;
-    setIsLoading(true);
 
     const getData = async () => {
       const data = await fetchSearchMovie(query);
-
       setMovies(data);
-      setIsLoading(false);
     };
     getData();
   }, [query]);
@@ -32,18 +27,6 @@ const MoviesPage = () => {
     searchParams.set("query", newQuery);
     setSearchParams(searchParams);
   };
-
-  if (isLoading) {
-    <Hourglass
-      visible={true}
-      height="80"
-      width="80"
-      ariaLabel="hourglass-loading"
-      wrapperStyle={{ marginTop: "50px" }}
-      wrapperClass=""
-      colors={["#306cce", "#72a1ed"]}
-    />;
-  }
 
   return (
     <div>
