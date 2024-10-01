6c51eae67acb0c999176529bfcab7152a5138e74
Kostia Zinchenko <pancostian@gmail.com>
2024-10-01 16:11:45 -0400

    updated

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,52 +1,21 @@
-import { useEffect, useState } from "react";
-import { fetchMovies } from "../../services/api";
 import { NavLink, useLocation } from "react-router-dom";
 import s from "./MovieList.module.css";
-import { Hourglass } from "react-loader-spinner";
 
 const MovieList = ({ movies }) => {
-  const [trendMovies, setTrendMovies] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
   const location = useLocation();
 
   const defaultImg =
     "https://dummyimage.com/400x600/cdcdcd/000.jpg&text=No+poster";
 
-  useEffect(() => {
-    setIsLoading(true);
-    const getAllMovies = async () => {
-      const data = await fetchMovies();
-      setTrendMovies(data);
-      setIsLoading(false);
-    };
-    getAllMovies();
-  }, []);
+  if (!movies) return;
 
-  const displayedMovies = movies ? movies : trendMovies;
-
-  if (isLoading)
-    return (
-      <Hourglass
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="hourglass-loading"
-        wrapperStyle={{ marginTop: "50px" }}
-        wrapperClass=""
-        colors={["#306cce", "#72a1ed"]}
-      />
-    );
-
-  if (!displayedMovies) return;
-
-  if (displayedMovies?.length === 0)
+  if (movies?.length === 0)
     return <h3>The movie you are searching is not here :/ </h3>;
 
   return (
     <div>
       <ul className={s.ul}>
-        {displayedMovies?.map((movie) => (
+        {movies?.map((movie) => (
           <li key={movie.id} className={s.li}>
             <NavLink
               className={s.linkWrapper}
